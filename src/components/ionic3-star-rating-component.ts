e701047fac43916189f8427fcf3a781fa49259c5
Lucas Ponte <luckpontecorreia@gmail.com>
2019-03-27 09:49:35 -0300

    Better conditional for the readonly flag Now the flag works for boolean 'true' values sent through the component.

--- src/components/ionic3-star-rating-component.ts
+++ src/components/ionic3-star-rating-component.ts
@@ -50,7 +50,7 @@ export class StarRating {
 
   changeRating(event){
 
-    if(this.readonly && this.readonly === "true") return;
+    if(this.readonly && this.readonly !== "false") return;
     // event is different for firefox and chrome
     this.rating = event.target.id ? parseInt(event.target.id) + 1 : parseInt(event.target.parentElement.id) + 1;
     // subscribe this event to get the changed value in ypour parent compoanent 
